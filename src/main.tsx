0ee8da7645ba99cc586d2f19353437fff5a9bd71
Davi Komura de Castro <davi.komura@gmail.com>
2025-07-04 17:00:41 +0100

    update vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import router from './routes.tsx';
 import './i18n.ts';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>,
 )
